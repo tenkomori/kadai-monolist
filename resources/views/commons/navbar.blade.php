e413754cd69d6afc2af608ec774eb07a9b906b3b
Takashi Ohmori <egao.wo.taisetuni@gmail.com>
2017-07-30 10:00:25 +0000

    re ranking

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,7 +9,7 @@ avbar-collapse-1" aria-expanded="false">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-left" href="/"><img src="images/logo.png" alt="Monolist"></a>
+                <a class="navbar-left" href="/"><img src="/images/logo.png" alt="Monolist"></a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
